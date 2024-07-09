09aa1ff3570eeaf48ab88d12e2df0f3105511367
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-09 10:45:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,41 @@
+let score = "Ankita"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+ //console.log(typeof run);
+//console.log(typeof(run));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber)
+    
+//console.log( valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = "Ankita"
+let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Ankita => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
