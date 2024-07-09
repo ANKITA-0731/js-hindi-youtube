0cf6d5500d96eaa508c5827f7b6219404a50aa2a
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-09 12:58:34 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,51 @@
+// primitive
+
+// 7 types : string,number,Boolean,null,undefined,symbol,BigInt
+
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let useremail;
+
+const id = Symbol('123');
+const anotherid = Symbol('123');
+console.log(id === anotherid);
+
+const BigInt = 345672864519n
+
+
+// reference type(non primitive)
+
+// array,objects,functions
+
+const heroes =  ["shaktimaan","salmaan","shahrukh"];
+
+let myObj = {
+    name: "ankita",
+    age: 22, 
+};
+
+const myFunction = function(){
+   console.log("Hello world");
+};
+
+
+console.log(typeof Bignumber);
+console.log(typeof outsideTemp);
+console.log(typeof scorevalue);
+console.log(typeof anotherid);
+console.log(typeof Myfunction);
+console.log(typeof array);
+console.log(typeof Object);
+console.log(typeof string);
+console.log(typeof undefined);
+console.log(typeof number);
+console.log(typeof Bignumber);
+
+
+
+   
+
+
