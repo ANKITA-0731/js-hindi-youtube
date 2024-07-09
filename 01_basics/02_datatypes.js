f830de69af59b863475afed0267f25b3619cc8bc
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-09 09:52:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,52 @@
+"use strict"; //treat all js code as never version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+ 3);
+    //code readiability should be high
+
+console.log("Ankita")
+
+let name ="ankita";
+let age = 20;
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
