a8c52d5ba8065adb58afc51a26b76153871a3af1
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-10 14:52:47 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,50 @@
+const score = 400;
+// console.log(score); 
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+
+const othernumber = 236.67867
+// console.log(othernumber.toPrecision(3)); 
+
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// +++++++++++++++++ MATHS ++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.round(45.67));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.max(4,9,12));
+// console.log(Math.sqrt(10));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +2);
+
+const min = 10
+const max = 20
+console.log((Math.floor(Math.random() * (max - min + 1) + min))); //important line 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
