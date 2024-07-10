01505514783a69b47a68c8a754468588a373462b
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-10 12:16:54 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,7 +11,7 @@ let useremail;
 
 const id = Symbol('123');
 const anotherid = Symbol('123');
-console.log(id === anotherid);
+// console.log(id === anotherid);
 
 const BigInt = 345672864519n
 
@@ -28,24 +28,42 @@ let myObj = {
 };
 
 const myFunction = function(){
-   console.log("Hello world");
+//    console.log("Hello world");
 };
 
 
-console.log(typeof Bignumber);
-console.log(typeof outsideTemp);
-console.log(typeof scorevalue);
-console.log(typeof anotherid);
-console.log(typeof Myfunction);
-console.log(typeof array);
-console.log(typeof Object);
-console.log(typeof string);
-console.log(typeof undefined);
-console.log(typeof number);
-console.log(typeof Bignumber);
+// console.log(typeof Bignumber);
+// console.log(typeof outsideTemp);
+// console.log(typeof scorevalue);
+// console.log(typeof anotherid);
+// console.log(typeof Myfunction);
+// console.log(typeof array);
+// console.log(typeof Object);
+// console.log(typeof string);
+// console.log(typeof undefined);
+// console.log(typeof number);
+// console.log(typeof Bignumber);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive)-used,heap(non-primitive)-used
+
+let myyoutube = "ankitasinghthakur"
+let anothername = myyoutube
+anothername = "chai aur code"
+console.log(myyoutube);
+console.log(anothername);
 
 
+let userOne =  {
+    email:  "user@google.com",
+    upi: "user@ybl"
+}
 
-   
+let userTwo = userOne
 
+userTwo.email = "ankita@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
