c240a0cb766c3eb5461110a3a8d62a360fa8439c
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-12 12:08:27 +0000

    objects

---
+++ 01_basics/02_basics/03_objects.js
@@ -0,0 +1,29 @@
+// most important topic 
+
+// ++++++++++++++++++ OBJECTS ++++++++++++++++++++ 
+
+// SINGLETON
+// OBJECT CREATE
+
+// Object literals
+
+const Jsuser = {
+     
+      name : "Ankita",
+      age : 20,
+      location : Satna ,
+      email: "ankita@gmail.com",
+      isLoggedIn : false,
+      lastLoginDays : ["monday","saturday"],
+                                             };
+
+     console.log(Jsuser.email);
+     console.log(Jsuser.age);
+
+     console.log(Jsuser["email"]);
+
+
+
+
+
+
