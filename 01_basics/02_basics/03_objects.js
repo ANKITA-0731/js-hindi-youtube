6a726a0c0eb829a9760dc943813f654289762eb0
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-16 14:48:58 +0000

    objects1

--- 01_basics/02_basics/03_objects.js
+++ 01_basics/02_basics/03_objects.js
@@ -7,9 +7,13 @@
 
 // Object literals
 
-const Jsuser = {
+const mySym = Symbol("key1");
+
+const  Jsuser = {
      
       name : "Ankita",
+      "full name ": "Ankita Singh singhorl",
+      [mySym]: "mykey1",
       age : 20,
       location : "Satna" ,
       email: "ankita@gmail.com",
@@ -17,10 +21,31 @@ const Jsuser = {
       lastLoginDays : ["monday","saturday"],
                                              };
 
-     console.log(Jsuser.email);
-     console.log(Jsuser.age);
+//      console.log(Jsuser.email);
+//      console.log(Jsuser.age);
 
-     console.log(Jsuser["email"]);
+//      console.log(Jsuser["email"]);
+//      console.log(Jsuser["full name "]);
+//      console.log(Jsuser[mySym]);
+
+     Jsuser.email = "ankita@gmail.com";
+//      Object.freeze(Jsuser);
+     Jsuser.email = "ankita@chatgpt.com";
+//      console.log(Jsuser);
+
+     Jsuser.greeting = function(){
+       console.log("Hello JS user");
+              }
+
+              
+
+              Jsuser.greetingTwo = function(){
+                  console.log('Hello JS user, ${this.name}');
+           
+                         console.log(Jsuser.greeting());
+                         console.log(Jsuser.greetingTwo());
+
+                
 
 
 
