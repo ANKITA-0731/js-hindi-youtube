ece46b678f47a19529af2ee228542db0784407f1
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-17 15:28:51 +0000

    functions

--- 01_basics/03_basics/01_functions.js
+++ 01_basics/03_basics/01_functions.js
@@ -18,12 +18,38 @@ function sayMyName (){
 
 // sayMyName();
 
-function addTwoNumbers (number1, number2){
-    console.log(number1 + number2);
-                                             }
+// function addTwoNumbers (number1, number2){
+    // console.log(number1 + number2);
+                                        // {    
 
 addTwoNumbers(10,2);
 addTwoNumbers(10,"2");
+addTwoNumbers(10,null);
+
+
+function addTwoNumbers (number1, number2)  {
+
+    // let result = number1 + number2
+    //  return result
+    return number1 + number2
+};
+    
+const result = addTwoNumbers(10,2);
+// console.log("result: ", result);
+
+function loginUserMessage(username = "sam"){
+
+    if(username === undefined){
+        console.log("please enter a username");
+        return
+    };
+
+    return '${username} just Logged in'
+};
+     console.log(loginUserMessage("ankita")); 
+     console.log(loginUserMessage("ankita")); 
+ 
+
 
 
 
