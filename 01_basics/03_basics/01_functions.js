6b7bc3d85b8b487e863f22721fedc759fca82e95
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-17 13:48:29 +0000

    functions

---
+++ 01_basics/03_basics/01_functions.js
@@ -0,0 +1,31 @@
+// console.log("A");
+// console.log("N");
+// console.log("K");
+// console.log("I");
+// console.log("T");
+// console.log("A");
+
+
+function sayMyName (){
+    console.log("A");
+    console.log("N");
+    console.log("K");
+    console.log("I");
+    console.log("T");
+    console.log("A");
+    
+}
+
+// sayMyName();
+
+function addTwoNumbers (number1, number2){
+    console.log(number1 + number2);
+                                             }
+
+addTwoNumbers(10,2);
+addTwoNumbers(10,"2");
+
+
+
+
+
