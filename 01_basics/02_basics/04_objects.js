dc4541f688d2da14be5ed695cdbbcd9a78103e63
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-17 11:05:45 +0000

    objects

--- 01_basics/02_basics/04_objects.js
+++ 01_basics/02_basics/04_objects.js
@@ -24,10 +24,84 @@ const regularuser = {
 
     // console.log(regularuser.fullname.userfullname.firstname);
 
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj4 = {5: "a", 6: "b"};
+
+   
+// const obj3 = {obj1 ,obj2};
+
+//  const obj3 = Object.assign({} ,obj1 , obj2,  obj4 );
+
+const obj3 ={...obj1, ...obj2}
+
+
+// console.log(obj3);
+
+const users = [
+   {
+       id : "1",
+       gamil : "yur@gmail.com",
+
+   },
+   { 
+   id : "1",
+   gamil : "yur@gmail.com",
+
+   },
+
+];
+
+
+
+users[1].email
+// console.log(tinderuser);
+
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
+
+// console.log(tinderuser.hasOwnProperty('IsLoggedIn'));
+ 
+const course = {
+       coursename : "js in hindi",
+       price : "999",
+       courseInstructor : "hitesh",
+};
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+// console.log(courseInstructor);
+
+// console.log(Instructor);
+
+
+// jSON
+
+// {
+//     "name" : "ankita",
+//     "coursename" : "js in hindi",
+//     "price": "free"
+// }
 
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
