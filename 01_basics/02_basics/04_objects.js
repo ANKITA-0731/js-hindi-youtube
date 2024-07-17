f31982497f494589da90c4927ef107e364e362e4
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-17 10:09:08 +0000

    objects a

--- 01_basics/02_basics/04_objects.js
+++ 01_basics/02_basics/04_objects.js
@@ -3,4 +3,31 @@
 // const tinderuser = new Object()
 
 const tinderuser = {}
-console.log(tinderuser);
+
+tinderuser.id ="123abc"
+tinderuser.name = "sam"
+tinderuser.IsLoggedIn = false
+
+// console.log(tinderuser);
+
+
+const regularuser = {
+        email: "ankita@yshoo.com",
+        fullname :{
+            userfullname:{
+                firstname : "Ankita",
+                lastname :  "Singhrol",
+            }
+        }
+    }
+
+
+    // console.log(regularuser.fullname.userfullname.firstname);
+
+
+
+
+
+
+
+    
