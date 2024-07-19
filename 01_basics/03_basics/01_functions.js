f30be6f1cc0f6961f2d0f7e12da4b84247354624
ANKITA-0731 <singhankita71906@gmail.com>
2024-07-19 15:22:48 +0000

    functions

--- 01_basics/03_basics/01_functions.js
+++ 01_basics/03_basics/01_functions.js
@@ -46,12 +46,40 @@ function loginUserMessage(username = "sam"){
 
     return '${username} just Logged in'
 };
-     console.log(loginUserMessage("ankita")); 
-     console.log(loginUserMessage("ankita")); 
+    //  console.log(loginUserMessage("ankita")); 
+    //  console.log(loginUserMessage("ankita")); 
  
 
+   function calculatecartprice ( val1,val2,...num1){
+        return num1                                //...rest of array
+    }
 
+// console.log(calculatecartprice(100,200,300));
 
+const user = {
+          username : "ankita",
+          price :"999"
 
+                } 
+
+function handleobject(anyobject) {
+    //    console.log('username is ${anyobject.username} and price is ${anyobject.price}');
+
+}
+
+// handleobject(user);
+handleobject({
+    username : "sam",
+    price : 199
+});
+
+const myNewArray = [200,300,400,500];
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,300,400,500]));
 
 
